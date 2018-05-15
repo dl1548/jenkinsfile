56a44d4bc45f2bdfc7cc240f8b8d90aa7bcac09e
lizili <dl1548@sina.com>
2018-05-15 09:55:20 +0800

    del tirgger

--- tirgger/src/com/zabbix/api/service/IMediaService.java
+++
@@ -1,5 +0,0 @@
-package com.zabbix.api.service;
-import com.zabbix.api.domain.media.MediaGetRequest;
-public interface IMediaService {
-	public Object get(MediaGetRequest get);
-}
--- tirgger/src/com/zabbix/api/test/Test.java
+++
@@ -1,9 +0,0 @@
-package com.zabbix.api.test;
-
-public class Test {
-
-	public static void main(String[] args) {
-		TestTrigger tt=new TestTrigger();
-		tt.testGet();
-	}
-}
